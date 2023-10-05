0c467cd9187fa2c3dd8d4a35c5c69328f307de0f
Mahdi Jaber <87242348+Mr6MJT@users.noreply.github.com>
2023-10-05 14:19:46 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 clear
 
 echo "Installing Requirments, It May Take Some Minutes, Please Wait..."
-sudo apt-get install procps && sudo apt-get install sysstat && sudo apt install busybox -y && sudo apt install coreutils && sudo apt-get install acl && sudo apt-get -y install parted && sudo apt-get -y install fdisk && sudo apt install pciutils && sudo apt-get install kmod && 
-
-sudo apt update
+sudo apt-get install procps && sudo apt-get install sysstat && sudo apt install busybox -y && sudo apt install coreutils && sudo apt-get install acl && sudo apt-get -y install parted && sudo apt-get -y install fdisk && sudo apt install pciutils && sudo apt-get install kmod && sudo apt update
 mv .st.sh start.sh
 clear
 bash start.sh
